b54e37294770ee2fc4fd4b4929e543dfbeaa587d
nban22 <nban22@clc.fitus.edu.vn>
2024-11-21 01:50:00 +0700

    changed the host to 0.0.0.0

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use('*', errorController.routesNotFound);
 app.use(errorController.globalErrorHandler);
 
 const PORT = parseInt(process.env.PORT || '3000', 10);
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
