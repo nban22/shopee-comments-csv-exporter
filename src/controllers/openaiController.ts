7bb45ea4e0e3604b2e316898d1a9d69db85c7d9f
nban22 <nban22@clc.fitus.edu.vn>
2024-11-22 00:59:07 +0700

    feat: add countdown timer for avaliable downlaod file

---
+++ src/controllers/openaiController.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+
+export const analyzeText = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    
+})
